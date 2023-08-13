7719d9e24d6247e644b95b4cfb0a96ce1d787632
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-08-13 17:55:35 +0300

    decor

---
+++ decorator.py
@@ -0,0 +1,17 @@
+lst = [1, 2, 3, 4, 5, 6, 7, 8]
+d = {"k":3, "l":4}
+
+def check_len(func):
+    def wrapper_decorator(*args, **kwargs):
+        if len(*args) > 10:
+            return "Too many"
+        value = len(*args)
+        value = value/2
+        return value    
+    return wrapper_decorator
+
+@check_len
+def my_func(lst):
+    return(len(lst))
+
+print(my_func(lst))
